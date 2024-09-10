2c142aa277838ba8a4b92c06e747af89729950a2
tyhuetter <thuetter@mail.pima.edu>
2024-09-10 11:48:41 -0700

    Update cis129_lab03_coffeeShop.py
    Put 2 more items on the menu, sandwiches and hot chocolate

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,14 +1,21 @@
+
 print('************************************')
 print('My Coffee and Muffin Shop')
 coffees = input('Number of coffees bought?\n')
 coffees = int(coffees)
 muffins = input('Number of muffins bought?\n')
 muffins = int(muffins)
+hotchocolates = input('Number of hot chocolates bought?\n')
+hchoc = int(hotchocolates)
+sandwiches = input('Number of sandwiches bought? \n')
+swich = int(sandwiches)
 
 coffeecost = coffees * 5
 muffincost = muffins * 4
-tax = (coffeecost + muffincost) * 0.06
-totalcost = coffeecost + muffincost + tax 
+hchoccost = hchoc * 3
+swichcost = swich * 6
+tax = (coffeecost + muffincost + hchoccost + swichcost) * 0.06
+totalcost = coffeecost + muffincost + hchoccost + swichcost + tax 
 print('************************************')
 print('                                     ')                                   
 print('************************************')
@@ -16,9 +23,16 @@ print('My Coffee and Muffin Shop Receipt')
 print(' ')
 print('', coffees, 'coffees for $5 each: $ ', coffeecost, '')
 print('', muffins,' muffins for $4 each: $ ', muffincost, '')
+print('', hchoc,' hotchocolates for $3 each: $ ', hchoccost, '')
+print('', swich,' sandwiches for $6 each: $ ', swichcost, '')
 print('6% tax : $ ', tax,'')
 print('-------------')
 print('Total: $ ', totalcost, '')
 print('***********************************')
+print(' ')
+print('Thank you for stopping by, have a wonderful day!')
+
+
+
 
 
