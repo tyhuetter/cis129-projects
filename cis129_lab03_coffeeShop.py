1b08916c8fe257733197326899cecc515537cca9
tyhuetter <thuetter@mail.pima.edu>
2024-09-12 09:58:21 -0700

    Update cis129_lab03_coffeeShop.py
    Added a thank you to the customer and a code for a free coffee next visit

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -20,5 +20,8 @@ print('6% tax : $ ', tax,'')
 print('-------------')
 print('Total: $ ', totalcost, '')
 print('***********************************')
-
+print(' ')
+print('Thank you stopping by, have a wonderful day')
+print('    ')
+print("USE THIS CODE FOR A FREE COFFEE NEXT VISIT: '7dclts53i'")
 
