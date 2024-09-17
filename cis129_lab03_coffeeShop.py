bdaedcddfc907924492503276aef4373e3e8d2e9
tyhuetter <thuetter@mail.pima.edu>
2024-09-16 19:34:24 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -30,7 +30,7 @@ print('-------------')
 print('Total: $ ', totalcost, '')
 print('***********************************')
 print(' ')
-=======
+print('=======')
 print('Thank you stopping by, have a wonderful day')
 print('    ')
 print("USE THIS CODE FOR A FREE COFFEE NEXT VISIT: '7dclts53i'")
